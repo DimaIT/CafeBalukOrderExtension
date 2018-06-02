651a620a8c52f37472c46cb109476f9f3afa6983
DimaIT <shellpixx@gmail.com>
2018-06-02 12:21:08 +0300

    add remove button

--- extension/script.js
+++ extension/script.js
@@ -2,39 +2,61 @@
 	if (window._initialized)
 		return
 
+    const template = `
+		<div class="shop-item__order-btn">
+        	<span class="btn btn-block _btn-add" > В корзину</span>
+        	<span class="btn btn-block _btn-remove" >
+        		<i class="fa fa-trash"></i>
+			</span>
+        </div>
+	`
+
 	let order = new Map()
 
-	function updateDom() {
+	function updateItems() {
 		Array.from(document.querySelectorAll('.shop-item'))
 			.filter(item => !item._updated)
 			.forEach(updateItem)
 	}
 
 	function updateItem(item) {
-		let btn = item.querySelector('.shop-item__order-btn')
-		btn.innerHTML = `<div class="shop-item__order-btn">
-                            <span class="btn btn-block" ></i> В корзину</span>
-                         </div>`
+		let container = item.querySelector('.shop-item__order-btn')
+		container.innerHTML = template;
+		let counter = item.querySelector('.shop-item__order')
+		counter.parentNode.removeChild(counter)
 
-		btn.onclick = () => {
+		let btnAdd = container.querySelector('._btn-add')
+        btnAdd.onclick = () => {
 			let description = item.querySelector('.shop-item__text').innerText
 			let price = item.querySelector('.shop-item__weight-price > dl > dd:nth-child(4)').innerText
 			price = parseFloat(price.trim().replace(',', '.').replace('р.', ''))
 			order.set(description, price)
 
-			let result = 0
-			let msg = ''
-			order.forEach((v, k) => {
-				result += v
-				msg += `${k} - ${v}p\n`
-			})
-			console.log(msg, result)
+			getResult()
+		}
+
+        let btnRemove = container.querySelector('._btn-remove')
+		btnRemove.onclick = () => {
+            let description = item.querySelector('.shop-item__text').innerText
+			order.delete(description)
+
+            getResult()
 		}
 
 		item._updated = true
 	}
 
-	setInterval(updateDom, 100) // to handle dom changes
+	function getResult() {
+        let result = 0
+        let msg = ''
+        order.forEach((v, k) => {
+            result += v
+            msg += `${k} - ${v}p\n`
+        })
+        console.log(msg, result)
+	}
+
+	setInterval(updateItems, 100) // to handle dom changes
 
 	window._initialized = true
 })()
